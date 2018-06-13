dc253a1dcf23782788652be69556d1fd00f676ca
Michael Kamau <michael.kamau@andela.com>
2018-06-13 13:06:34 +0300

    Add test - add single note with title and content

--- omniNotes/src/androidTest/java/it/feio/android/omninotes/NotesActionsTest.java
+++ omniNotes/src/androidTest/java/it/feio/android/omninotes/NotesActionsTest.java
@@ -1,6 +1,7 @@
 package it.feio.android.omninotes;
 
 
+import android.support.test.espresso.Espresso;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -10,9 +11,13 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 @LargeTest
@@ -20,7 +25,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 public class NotesActionsTest extends BaseEspressoTest {
 
     @Test
-    public void tapFab_DisplaysCreateNoteOptionsButtons(){
+    public void tapFab_DisplaysCreateNoteOptionsButtons() {
         final ViewInteraction fabInteractor = onView(withId(R.id.fab));
         fabInteractor.check(matches(isDisplayed()));
         fabInteractor.perform(click());
@@ -29,5 +34,23 @@ public class NotesActionsTest extends BaseEspressoTest {
         onView(withId(R.id.fab_checklist)).check(matches(isDisplayed()));
         onView(withId(R.id.fab_camera)).check(matches(isDisplayed()));
 
+    }
+
+    @Test
+    public void addSingleNoteWithTitleAndContent() {
+        final String note_title = "Note Title";
+        final String note_contents = "Note Contents";
+        openAddTextNoteView();
+        onView(withId(R.id.detail_title)).perform(typeText(note_title), closeSoftKeyboard());
+        onView(withId(R.id.detail_content)).perform(typeText(note_contents), closeSoftKeyboard());
+        Espresso.pressBack();
+        // verify that new note is in the notes list
+        onView(withId(R.id.list)).check(matches(hasDescendant(withText(note_title))));
+        onView(withId(R.id.list)).check(matches(hasDescendant(withText(note_contents))));
+    }
+
+    private void openAddTextNoteView() {
+        onView(withId(R.id.fab_expand_menu_button)).perform(click());
+        onView(withId(R.id.fab_note)).perform(click());
     }
 }
